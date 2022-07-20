94aa027466481a2688cfcbbf618e7a2ddb4d069d
DavidLiu <david.yx.liu@oracle.com>
2022-07-20 11:49:45 +0800

    [WIP] pypi publish

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='python_utils',
+    author='david-khala@hotmail.com',
+    version='0.0.0',
+    packages=find_packages('.'),
+)
