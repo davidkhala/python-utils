3a7da1a8f93efd0a6e5c2fb82e3a9210c7db7d58
davidliu <david-khala@hotmail.com>
2022-07-24 12:08:33 +0800

    [DONE] basic auth

--- http_request/__init__.py
+++ http_request/__init__.py
@@ -1,9 +1,17 @@
 import requests
+from requests.auth import HTTPBasicAuth
 
 
-def get(url, params=None):
-    return requests.get(url, params).json()
+class Request:
+    def __init__(self, url, auth=None):
+        self.url = url
+        if auth is not None:
+            self.auth = HTTPBasicAuth(auth['username'], auth['password'])
 
+    def get(self, params=None, **kwargs):
+        kwargs.setdefault('auth', getattr(self, 'auth', None))
+        return requests.get(self.url, params, **kwargs).json()
 
-def post(url, json=None, data=None):
-    return requests.post(url, data, json).json()
+    def post(self, json=None, data=None, **kwargs):
+        kwargs.setdefault('auth', getattr(self, 'auth', None))
+        return requests.post(self.url, data, json).json()
--- http_request/test/unit.py
+++ http_request/test/unit.py
@@ -1,20 +1,38 @@
 import unittest
-from http_request import get, post
+from http_request import Request
 
 
 class TestHttpBin(unittest.TestCase):
     def test_your_ip(self):
-        response = get('https://httpbin.org/ip')
+        url = 'https://httpbin.org/ip'
+        request = Request(url)
+        response = request.get()
         print('Your IP is {0}'.format(response['origin']))
 
+    def test_basic_auth(self):
+        #     /basic-auth/{user}/{passwd}
+        url = 'https://httpbin.org/basic-auth/username/password'
+        auth = {
+            'username': 'username',
+            'password': 'password'
+        }
+
+        request = Request(url, auth)
+        response = request.get()
+        print(response)
+
     def test_get_with_load(self):
+        url = 'https://httpbin.org/get'
+        request = Request(url)
         payload = {'things': 2, 'total': 25}
-        r = get('https://httpbin.org/get', payload)
+        r = request.get(payload)
         print(r)
 
     def test_post(self):
+        url = 'https://httpbin.org/post'
+        request = Request(url)
         payload = {'username': 'Olivia', 'password': '123'}
-        r = post('https://httpbin.org/post', payload)
+        r = request.post(payload)
         print(r)
 
 
