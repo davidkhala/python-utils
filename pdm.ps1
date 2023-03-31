ed5caa506fabaaa615f1301710fc3f10292840f8
David Liu <david-khala@hotmail.com>
2023-03-31 12:46:09 +0800

    Create pdm.ps1

---
+++ pdm.ps1
@@ -0,0 +1,14 @@
+function Install-PDM {
+
+  (Invoke-WebRequest -Uri https://raw.githubusercontent.com/pdm-project/pdm/main/install-pdm.py -UseBasicParsing).Content | python3 -
+}
+
+function Uninstall-PDM {
+
+  (Invoke-WebRequest -Uri https://raw.githubusercontent.com/pdm-project/pdm/main/install-pdm.py -UseBasicParsing).Content | python3 -
+  # --remove
+}
+
+
+
+
