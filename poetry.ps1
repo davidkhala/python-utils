68ceeb24c8bfc5283e2de96b0fcd64e62e6b7c1a
David Liu <david-khala@hotmail.com>
2023-04-11 21:38:07 +0800

    Update poetry.ps1

--- poetry.ps1
+++ poetry.ps1
@@ -1,3 +1,8 @@
 function Install-Poetry {
   (Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | py -
+  
+  (Invoke-WebRequest https://raw.githubusercontent.com/davidkhala/windows-utils/master/powershell.ps1).Content | Add-Path ()
+}
+function Uninstall-Poetry {
+  (Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | py - --uninstall
 }
