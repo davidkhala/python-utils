d87ef37478115bc19504b3c11452737c7c0808aa
David Liu <david.yx.liu@oracle.com>
2023-04-24 22:41:03 +0800

    Add which-poetry

--- poetry/poetry.ps1
+++ poetry/poetry.ps1
@@ -1,11 +1,17 @@
-function Install-Poetry {
-  (Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | py -
-  Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
-  (Invoke-WebRequest -Uri https://raw.githubusercontent.com/davidkhala/windows-utils/master/powershell.ps1 -UseBasicParsing).Content | Invoke-Expression
-  Add-Path %APPDATA%\Python\Scripts
-  poetry --version
+function Install-Poetry
+{
+    (Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | py -
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+    (Invoke-WebRequest -Uri https://raw.githubusercontent.com/davidkhala/windows-utils/master/powershell.ps1 -UseBasicParsing).Content | Invoke-Expression
+    Add-Path %APPDATA%\Python\Scripts
+    poetry --version
 
 }
-function Uninstall-Poetry {
-  (Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | py - --uninstall
+function Uninstall-Poetry
+{
+    (Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | py - --uninstall
+}
+function Which-Poetry
+{
+    Get-Command poetry
 }
