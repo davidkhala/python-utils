f9b603059413f490ca9575f49fa1c8f0534a049c
David Liu <david-khala@hotmail.com>
2023-10-25 02:58:27 +0800

    Update poetry.ps1

--- poetry/poetry.ps1
+++ poetry/poetry.ps1
@@ -3,9 +3,8 @@ function Install-Poetry
     (Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | python -
     Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
     (Invoke-WebRequest -Uri https://raw.githubusercontent.com/davidkhala/windows-utils/master/powershell.ps1 -UseBasicParsing).Content | Invoke-Expression
-    # Add-Path $env:APPDATA\Python\Scripts
-    # TODO navigate to folder of $python and concat it with `Scripts`, instead of hardcode
-
+    Add-Path $env:APPDATA\Python\Scripts
+    
     poetry --version
 
 }
