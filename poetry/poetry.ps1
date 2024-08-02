83774afb0c28c03ca418a6a71afd7eed95185192
David Liu <david-khala@hotmail.com>
2024-08-02 08:20:34 +0800

    Update poetry.ps1

--- poetry/poetry.ps1
+++ poetry/poetry.ps1
@@ -3,7 +3,7 @@ function Install-Poetry
     (Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | py -
     Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
     (Invoke-WebRequest -Uri https://raw.githubusercontent.com/davidkhala/windows-utils/master/powershell.ps1 -UseBasicParsing).Content | Invoke-Expression
-    Add-Path $env:APPDATA\Python\Scripts
+    Add-Path $env:APPDATA\Python\Scripts # need admin
     
     poetry --version
 
