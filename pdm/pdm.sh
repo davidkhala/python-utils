b1d7fe3b012400fa87c3fe8703ca45413a5a97c7
David Liu <david-khala@hotmail.com>
2024-12-03 07:27:01 +0800

    publish 0.0.8

--- pdm/pdm.sh
+++
@@ -1,7 +0,0 @@
-install() {
-    curl -sSL https://raw.githubusercontent.com/pdm-project/pdm/main/install-pdm.py | python3 -
-}
-uninstall() {
-    curl -sSL https://raw.githubusercontent.com/pdm-project/pdm/main/install-pdm.py | python3 - --remove
-}
-$@
