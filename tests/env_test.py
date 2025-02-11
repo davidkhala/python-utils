cb36d19408b86862d8bda58ae74aef512ea6ca8e
David Liu <david-khala@hotmail.com>
2025-02-11 09:22:57 +0000

    close Use python to call cmd #2

--- tests/env_test.py
+++ tests/env_test.py
@@ -5,22 +5,30 @@ from dotenv import load_dotenv
 
 class EnvTestCase(unittest.TestCase):
     def test_load(self):
-        load_dotenv(dotenv_path='tests/data/.env')
-        domain = os.getenv('domain')
+        load_dotenv(dotenv_path="tests/data/.env")
+        domain = os.getenv("domain")
 
-        self.assertEqual(domain, 'example.org')
+        self.assertEqual(domain, "example.org")
 
     def test_env_cannot_overwrite(self):
-        os.putenv('domain', 'overwrite')
-        load_dotenv(override=False, dotenv_path='tests/data/.env')
-        domain = os.getenv('domain')
+        os.putenv("domain", "overwrite")
+        load_dotenv(override=False, dotenv_path="tests/data/.env")
+        domain = os.getenv("domain")
 
-        self.assertEqual(domain, 'example.org')
+        self.assertEqual(domain, "example.org")
 
     def test_env_list_all(self):
         for name, value in os.environ.items():
             print("{0}: {1}".format(name, value))
 
 
-if __name__ == '__main__':
+class SystemTestCase(unittest.TestCase):
+    def test_ls(self):
+
+        import subprocess
+
+        subprocess.run(["ls"])
+
+
+if __name__ == "__main__":
     unittest.main()
