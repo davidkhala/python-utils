c56df3037d224cf2c715ef25a96b66aca014654b
David Liu <david-khala@hotmail.com>
2025-02-11 20:50:27 +0800

    Update admin.zsh

--- poetry/admin.zsh
+++ poetry/admin.zsh
@@ -1,12 +1,13 @@
 set -e
 install() {
+  pip3 install certifi
   curl -sSL https://install.python-poetry.org | python3 -
   # https://python-poetry.org/docs/main/#enable-tab-completion-for-bash-fish-or-zsh
+  echo "fpath+=~/.zfunc" > ~/.zshrc
+  echo "autoload -Uz compinit && compinit" >/.zshrc
+  mkdir  ~/.zfunc/
   poetry completions zsh > ~/.zfunc/_poetry
-  # TODO
-  # You must then add the following lines in your ~/.zshrc, if they do not already exist:
-  #   fpath+=~/.zfunc
-  #   autoload -Uz compinit && compinit
+
 }
 "$@"
 
