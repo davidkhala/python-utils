8e6bad42eb9983130933aa701047fa4ef2e84a09
David Liu <david-khala@hotmail.com>
2025-02-11 21:01:21 +0800

    Update admin.zsh

--- poetry/admin.zsh
+++ poetry/admin.zsh
@@ -1,6 +1,6 @@
 set -e
 install() {
-  pip3 install certifi
+  curl https://raw.githubusercontent.com/davidkhala/mac-utils/refs/heads/master/language/python/313/Install%20Certificates.command | sh
   curl -sSL https://install.python-poetry.org | python3 -
   # https://python-poetry.org/docs/main/#enable-tab-completion-for-bash-fish-or-zsh
   echo "fpath+=~/.zfunc" > ~/.zshrc
