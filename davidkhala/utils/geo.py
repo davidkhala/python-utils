b417e431eecf2f9d596f60dd416d8e3b05105a7f
David Liu <david-khala@hotmail.com>
2025-10-31 09:04:31 +0800

    add precision

--- davidkhala/utils/geo.py
+++ davidkhala/utils/geo.py
@@ -1,12 +1,21 @@
 import numpy
+
+
 def as_of(lat, lon):
     return numpy.array([lat, lon])
 
-radius=6371000 # of earth, in meter
+
+earth_radius = 6371000  # of earth, in meter
+
+
 def xyz_of(lat, lon):
     lat = numpy.radians(lat)
     lon = numpy.radians(lon)
-    x = radius * numpy.cos(lat) * numpy.cos(lon)
-    y = radius * numpy.cos(lat) * numpy.sin(lon)
-    z = radius * numpy.sin(lat)
+    x = earth_radius * numpy.cos(lat) * numpy.cos(lon)
+    y = earth_radius * numpy.cos(lat) * numpy.sin(lon)
+    z = earth_radius * numpy.sin(lat)
     return numpy.array([x, y, z])
+
+
+def precision_cutoff(lat_or_lon: float):
+    return format(lat_or_lon, '.6f')
