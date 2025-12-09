34849c8c8146aa9842b1284684cca4446051e008
David Liu <david-khala@hotmail.com>
2025-12-09 11:42:24 +0800

    davidkhala.utils:0.6.6 use borrowed way

--- davidkhala/utils/http_request/stream.py
+++ davidkhala/utils/http_request/stream.py
@@ -1,19 +1,20 @@
 import requests
 from typing import Generator
 import json
-from davidkhala.utils.http_request import Request as SessionRequest
 
+from requests import Session
+
+from davidkhala.utils.http_request import Request as SessionRequest
 
 
 def as_sse(response: requests.Response) -> Generator[dict, None, None]:
     return (json.loads(line[5:].decode()) for line in response.iter_lines() if line)
 
 
-class Request(SessionRequest):
-    def __init__(self, auth: dict = None):
-        super().__init__(auth)
-        del self.on_response
-
+class Request:
+    def __init__(self, borrow: SessionRequest):
+        self.options: dict = borrow.options
+        self.session: Session = borrow.session
 
     def request(self, url, method: str, params=None, data=None, json=None) -> requests.Response:
-        return self.session.request(method, url, stream= True, params=params, data=data, json=json, **self.options)
+        return self.session.request(method, url, stream=True, params=params, data=data, json=json, **self.options)
